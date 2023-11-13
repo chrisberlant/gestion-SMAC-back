c389951d64e282ac44a78a542fde6b27219674b7
Chris Berlant <chris.berlant@gmail.com>
2023-11-14 00:17:03 +0100

    updated data validation middleware to use zod safeParse

--- app/middlewares/dataValidationMiddleware.ts
+++ app/middlewares/dataValidationMiddleware.ts
@@ -10,24 +10,21 @@ const dataValidation =
 	(schema: ValidationSchema) =>
 	(req: UserRequest, res: Response, next: NextFunction) => {
 		// We check here if user request is GET or any other type to either validate the req.params or the req.body
-		try {
-			if (req.method === 'GET') {
-				req.params = schema.parse(req.params);
-				for (const key in req.params) {
-					req.params[key] = xss(req.params[key]);
-				}
-			} else {
-				req.body = schema.parse(req.body);
-				for (const key in req.body) {
-					// Each value besides the password will be sanitized from malicious inserts
-					if (key !== 'password') req.body[key] = xss(req.body[key]);
-				}
+		if (req.method === 'GET') {
+			const result = schema.safeParse(req.params);
+			if (!result.success) return res.status(400).json(result.error);
+			for (const key in req.params) {
+				req.params[key] = xss(req.params[key]);
 			}
-			next();
-		} catch (error) {
-			if (error instanceof ZodError)
-				return res.status(400).json(error.issues[0].message);
+		} else {
+			const result = schema.safeParse(req.body);
+			if (!result.success) return res.status(400).json(result.error);
+			for (const key in req.body) {
+				// Each value besides the password will be sanitized from malicious inserts
+				if (key !== 'password') req.body[key] = xss(req.body[key]);
+			}
 		}
+		next();
 	};
 
 export default dataValidation;
