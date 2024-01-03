62c1b27af1eeb5dad3bebbbbc9e7017220c97590
Chris Berlant <chris.berlant@gmail.com>
2024-01-03 01:19:54 +0100

    added an else statement in data validation when id does not exist

--- app/middlewares/dataValidationMiddleware.ts
+++ app/middlewares/dataValidationMiddleware.ts
@@ -10,11 +10,17 @@ const dataValidation =
 	(req: UserRequest, res: Response, next: NextFunction) => {
 		// We check here if user request is GET or any other type to either validate the req.params or the req.body
 		if (req.method === 'GET') {
-			// The id is converted to a number to be checked in the validation schema
-			const convertedId = Number(req.params.id);
-			const result = schema.safeParse({ id: convertedId });
-			if (!result.success)
-				return res.status(400).json(result.error.issues[0].message);
+			if (req.params.id) {
+				// The id is converted to a number to be checked in the validation schema
+				const convertedId = Number(req.params.id);
+				const result = schema.safeParse({ id: convertedId });
+				if (!result.success)
+					return res.status(400).json(result.error.issues[0].message);
+			} else {
+				const result = schema.safeParse(req.params);
+				if (!result.success)
+					return res.status(400).json(result.error.issues[0].message);
+			}
 		} else {
 			// Methods different from GET, so it must contain a body
 			if (Object.keys(req.body).length === 0)
